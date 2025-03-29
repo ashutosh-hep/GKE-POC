5b4ec6c7e57c4c390a2d11720a9be1848767b69d
Ashutosh Kumar <63195712+ashutosh-hep@users.noreply.github.com>
2025-03-29 15:18:52 +0530

    Update prometheus-grafana-bootstrap.sh

--- prometheus-grafana-bootstrap.sh
+++ prometheus-grafana-bootstrap.sh
@@ -1,22 +1,22 @@
 echo "starting to provision"
 
-kubectl create ns mn
+kubectl create ns monitor
 
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
-helm install prometheus prometheus-community/prometheus -n mn
+helm install prometheus prometheus-community/prometheus -n monitor
 
-kubectl expose service prometheus-server --type=NodePort --target-port=9090 --name=prometheus-server-ext -n mn
+kubectl expose service prometheus-server --type=NodePort --target-port=9090 --name=prometheus-server-ext -n monitor
 
 helm repo add grafana https://grafana.github.io/helm-charts 
 helm repo update
 
-helm install grafana grafana/grafana -n mn
+helm install grafana grafana/grafana -n monitor
 
-kubectl expose service grafana --type=NodePort --target-port=3000 --name=grafana-ext -n mn
+kubectl expose service grafana --type=NodePort --target-port=3000 --name=grafana-ext -n monitor
 
-kubectl get svc
+kubectl get svc -n monitor
 echo "use port starting like 300000"
 
 echo "setup completed"
